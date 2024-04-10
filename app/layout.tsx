b98288667362fd57176fd5297ead80272a6dac1c
Slavinto <slava3669@gmail.com>
2024-04-10 17:23:28 +0300

    fixed some minor color bugs in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
                     colorPrimary: "#0E78F9",
                     colorInputBackground: "#252A41",
                     colorInputText: "#FFF",
-                    colorTextSecondary: "",
+                    // colorTextSecondary: "#FFF",
+                    colorTextOnPrimaryBackground: "#FFF",
+                    colorSuccess: "#23f234",
+                    colorNeutral: "#787878",
                 },
             }}
         >
