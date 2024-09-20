0b29d8e610cc2b0c327f37ec239a93dfba76f45f
Slavinto <slava3669@gmail.com>
2024-09-20 11:22:16 +0300

    added generatestaticparams dummy function to sign-in dynamic route

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
+export function generateStaticParams() {
+    return [{}];
+}
+
 export default function SignInPage() {
     return (
         <section className='flex w-full h-lvh justify-center items-center'>
